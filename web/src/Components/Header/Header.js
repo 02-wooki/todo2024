f15ca85afc514d84f02ca81716145bf1cdf30e04
Wooki <hyunook02@naver.com>
2024-08-15 20:56:57 +0900

    반응형 디자인
    - 모바일 뷰 대응

--- web/src/Components/Header/Header.js
+++ web/src/Components/Header/Header.js
@@ -25,25 +25,28 @@ export default function Header() {
                 className={'profile-pic' + modalState}
                 src="https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg"
                 onMouseEnter={() => modalOpenHandler(true)}
+                onClick={() => console.log('profile')}
             />
             <div
                 className={'div-modal' + modalState}
                 onTouchEnd={() => modalOpenHandler(true)}
                 onMouseLeave={() => modalOpenHandler(false)}
             />
-            { modalOpen === true ?
+            { modalOpen === true || window.innerWidth < 768 ?
                 <div>
                     <BiCog
                         id='settings-icon'
                         size={45}
                         color='#444'
                         onMouseEnter={() => modalOpenHandler(true)}
+                        onClick={() => console.log('setting')}
                     />
                     <BiLogOut
                         id='logout-icon'
                         size={45}
                         color='#444'
                         onMouseEnter={() => modalOpenHandler(true)}
+                        onClick={() => console.log('logout')}
                     />
                 </div> : null
             }
