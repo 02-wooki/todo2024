645fa09bd333d41c107ed4d8d9215ff04b1499c4
Wooki <hyunook02@naver.com>
2024-08-15 18:38:40 +0900

    반응형 디자인
    - 태블릿 대응 (768~1024px) 뷰와 모니터 대응 (1024px~) 대응 뷰 구분

--- web/src/Components/Header/Header.js
+++ web/src/Components/Header/Header.js
@@ -7,58 +7,45 @@ import { BiPlusCircle, BiLogOut, BiCog } from "react-icons/bi";
 export default function Header() {
 
     const [modalOpen, setmodalOpen] = useState(false);
-    const [settingsExplation, setSettingsExplation] = useState(false);
-    const [logoutExplation, setLogoutExplation] = useState(false);
-
+    const [modalState, setModalState] = useState('');
+    
+    const modalOpenHandler = (stat) => {
+        if(stat) {
+            setmodalOpen(true);
+            setModalState(' modalOpen');
+        } else {
+            setmodalOpen(false);
+            setModalState('');
+        }
+    }
+    
     return(
         <div className='header'>
             <img
-                className='profile-pic'
+                className={'profile-pic' + modalState}
                 src="https://health.chosun.com/site/data/img_dir/2023/07/17/2023071701753_0.jpg"
-                onMouseEnter={() => setmodalOpen(true)}
+                onMouseEnter={() => modalOpenHandler(true)}
             />
-            {modalOpen === true ?
-                <div
-                    className='div-modal'
-                    id='modalopen-profileback'
-                    onMouseLeave={() => setmodalOpen(false)}
-                >
+            <div
+                className={'div-modal' + modalState}
+                onTouchEnd={() => modalOpenHandler(true)}
+                onMouseLeave={() => modalOpenHandler(false)}
+            />
+            { modalOpen === true ?
+                <div>
                     <BiCog
                         id='settings-icon'
                         size={45}
                         color='#444'
-                        onMouseEnter={() => setSettingsExplation(true)}
+                        onMouseEnter={() => modalOpenHandler(true)}
                     />
                     <BiLogOut
                         id='logout-icon'
                         size={45}
                         color='#444'
-                        onMouseEnter={() => setLogoutExplation(true)}
+                        onMouseEnter={() => modalOpenHandler(true)}
                     />
-                </div> :
-                <div
-                    className='div-modal'
-                    id='modalclose-profileback'
-                    onMouseEnter={() => setmodalOpen(true)}
-                />
-            }
-            {settingsExplation === true ?
-                <div
-                    className='icon-oncursor'
-                    id='settings-oncursor'
-                    onMouseOver={() => setmodalOpen(true)}
-                    onMouseOut={() => setSettingsExplation(false)}>
-                        설정
-                </div> :<></>
-            }
-            {logoutExplation === true ?
-                <div
-                    className='icon-oncursor'
-                    id='logout-oncursor'
-                    onMouseOver={() => setmodalOpen(true)}
-                    onMouseOut={() => setLogoutExplation(false)}>
-                    로그아웃
-                </div> :<></>
+                </div> : null
             }
 
         </div>
