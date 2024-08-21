babf2b042be4928f7ea7d71cb3a3a5e433f5d99f
Wooki <hyunook02@naver.com>
2024-08-22 02:54:46 +0900

    DB 삽입 기능 구현
    - 코드 조금 더 다듬을 것
    - 백엔드 폴더에 mysql2 모듈 설치

--- back/index.js
+++ back/index.js
@@ -1,27 +1,47 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const mariadb = require('./database/mariadb');
 
 const app = express();
 const port = 8080;
 
-app.use(express.static(path.join('../web/build')));
+app.use(express.static(path.join(__dirname, '../web/build')));
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function (req, res) {
-    res.sendFile(path.join('../web/build/index.html'));
-})
+mariadb.connect();
+function pushHandler (body) {
+    mariadb.query(`insert into lists(bookId, checked, content) 
+        values(${body.bookId}, ${body.check}, '${body.content}')`,
+    function (err, rows) { console.log(rows); });
+}
 
-app.post('/api/getlist', function (req, res) {
-    const body = req.body;
-    console.log(`${body.content}`);
-    
+// app.get('/', function (req, res) {
+//     res.sendFile(path.join(__dirname, '../web/build/index.html'));
+// })
+
+app.post('/api/putlist', function (req, res) {
+    console.log(`${req.body.bookId}: ${req.body.content}`);
+    pushHandler(req.body);
     res.json({ body : 'OK' });
 })
 
 app.post('/api/updatelist', function (req, res) {
     
+})
+
+app.get('api/getlist', function (req, res) {
+    var result;
+    const q = req.query;
+
+    if(q.all) {
+        mariadb.query(`select * from lists`, function (err, rows) {
+            result = rows;
+        })
+    }
+
+    res.json({ body : rows });
 })
 
 app.listen(port, () => {
