48f745afad06190f026900b25fc38df77796e498
Wooki <hyunook02@naver.com>
2024-08-26 18:17:46 +0900

    fixed issue
    - fix #1 : add list 쿼리문 오류 수정

--- back/controllers/post/addlist.js
+++ back/controllers/post/addlist.js
@@ -3,15 +3,18 @@ const mariadb = require('../../database/mariadb');
 const addlist = (req, res) => {
 
     if (req.body.content.length === 0)
-        req.send({ body : { status : 'NOT OK'} });
+        req.send({ body : { status : 'NOT OK', content : 'request content length is 0' } });
 
     mariadb.connect();
-    mariadb.query(`insert into lists (content) values (${req.body.content});`, (err, rows) => {
+    mariadb.query(`insert into lists (content) values ('${req.body.content}');`, (err, rows) => {
         if (err)
-            res.send({ body : { status : 'NOT OK'} });
+            res.send({ body : { status : 'NOT OK', content : err } });
         else
             mariadb.query(`select * from lists`, (err, rows) => {
-                res.send({ body : { status : 'OK', content : rows} });
+                if (err)
+                    res.send({ body : { status : 'NOT OK', content : err } });
+                else
+                    res.send({ body : { status : 'OK', content : rows} });
             })
     });
 }
